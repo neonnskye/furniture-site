d369cd1f9243baeb8ce1654044cccd4de5708e3b
neonnskye <theneonnofficial@gmail.com>
2026-01-11 22:06:59 +0530

    Add navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Navbar from "./lib/Navbar.svelte";
 </script>
 
 <main>
+  <Navbar />
   <div>
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -20,12 +22,14 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,11 @@
+<nav class="flex justify-between uppercase p-5 font-playfair">
+  <div class="">
+    <h2>Untitled Furniture</h2>
+  </div>
+  <div class="">
+    <a href="#top">Home</a>
+    <a href="#top">About</a>
+    <a href="#top">Portfolio</a>
+    <a href="#top">Contact</a>
+  </div>
+</nav>
