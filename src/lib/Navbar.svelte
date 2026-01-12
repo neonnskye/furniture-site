831bef440a7af1efb0119fb8aca9d99fc80ad0fe
neonnskye <theneonnofficial@gmail.com>
2026-01-12 21:16:32 +0530

    Update Navbar styles

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,13 @@
-<nav class="font-playfair-display flex justify-between p-5 uppercase">
+<nav
+  class="font-playfair-display mx-20 my-5 flex justify-between text-lg uppercase"
+>
   <div class="">
-    <h2>Untitled Furniture</h2>
+    <h2 class="tracking-widest">Untitled Furniture</h2>
   </div>
-  <div class="">
-    <a href="#top">Home</a>
-    <a href="#top">About</a>
-    <a href="#top">Portfolio</a>
-    <a href="#top">Contact</a>
+  <div class="text-gray tracking-wide">
+    <a href="#top" class="mx-1 hover:text-black">Home</a>
+    <a href="#top" class="mx-1 hover:text-black">About</a>
+    <a href="#top" class="mx-1 hover:text-black">Portfolio</a>
+    <a href="#top" class="mx-1 hover:text-black">Contact</a>
   </div>
 </nav>
