ad8262a6b95ef3ae8b3aad020e68ae73147d9a97
neonnskye <theneonnofficial@gmail.com>
2026-01-12 21:16:48 +0530

    Add Hero component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Navbar from "./lib/Navbar.svelte";
+  import Hero from "./lib/Hero.svelte";
 </script>
 
 <Navbar />
+<Hero />
---
+++ src/lib/Hero.svelte
@@ -0,0 +1,13 @@
+<div class="mx-20 flex h-150 justify-center bg-[url(/hero.jpg)] bg-center">
+  <div class="self-center text-center text-white uppercase">
+    <div class="font-raleway tracking-[0.5rem]">
+      <h2 class="mb-5 text-5xl font-semibold">Untitled Furniture</h2>
+      <h3 class="mb-5 text-3xl font-light">Wood & Iron Works</h3>
+    </div>
+    <div class="font-playfair-display">
+      <a href="#top" class="bg-white px-3 py-2 text-lg tracking-wide text-black"
+        >Get a Quote</a
+      >
+    </div>
+  </div>
+</div>
