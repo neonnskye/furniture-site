e94aa6c4f7bc5e5fed8a55001ca4901a04c1ce16
neonnskye <theneonnofficial@gmail.com>
2026-01-13 20:12:36 +0530

    Add Portfolio component and images

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Navbar from "./lib/Navbar.svelte";
   import Hero from "./lib/Hero.svelte";
   import About from "./lib/About.svelte";
+  import Portfolio from "./lib/Portfolio.svelte";
 </script>
 
 <main class="mx-20">
   <Navbar />
   <Hero />
   <About />
+  <Portfolio />
 </main>
---
+++ src/lib/Portfolio.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import bedImage from "/src/assets/bed.jpg";
+  import tableImage from "/src/assets/table.jpg";
+  import stairImage from "/src/assets/stairs.jpg";
+  const images = [bedImage, tableImage, stairImage];
+</script>
+
+<section>
+  <div>
+    <h2>Potfolio</h2>
+  </div>
+  <div>
+    {#each images as image}
+      <img src={image} alt="" />
+    {/each}
+  </div>
+</section>
