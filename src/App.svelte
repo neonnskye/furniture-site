7bacad9791435aa339031375faad64c5dadb7ba3
neonnskye <theneonnofficial@gmail.com>
2026-01-13 18:46:46 +0530

    Add About component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Navbar from "./lib/Navbar.svelte";
   import Hero from "./lib/Hero.svelte";
+  import About from "./lib/About.svelte";
 </script>
 
 <Navbar />
 <Hero />
+<About />
---
+++ src/lib/About.svelte
@@ -0,0 +1,10 @@
+<section>
+  <h2>About</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor
+    imperdiet enim sed scelerisque. Vivamus pulvinar ultrices neque. Proin
+    molestie diam sed commodo vehicula. Pellentesque at dignissim ex. Donec eu
+    arcu massa. Nunc scelerisque et nibh at volutpat. Nullam eget libero
+    pulvinar diam iaculis dictum. Vestibulum hendrerit non enim a.
+  </p>
+</section>
