58af925a4fcc22dc01138f3591e8d0bf657f8121
neonnskye <theneonnofficial@gmail.com>
2026-01-13 19:13:12 +0530

    Style About component

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,10 +1,14 @@
-<section>
-  <h2>About</h2>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor
-    imperdiet enim sed scelerisque. Vivamus pulvinar ultrices neque. Proin
-    molestie diam sed commodo vehicula. Pellentesque at dignissim ex. Donec eu
-    arcu massa. Nunc scelerisque et nibh at volutpat. Nullam eget libero
-    pulvinar diam iaculis dictum. Vestibulum hendrerit non enim a.
-  </p>
+<section class="mb-5 text-center">
+  <div class="mx-auto w-180">
+    <h2 class="font-playfair-display mb-5 text-2xl tracking-wide uppercase">
+      About
+    </h2>
+    <p class="font-raleway">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor
+      imperdiet enim sed scelerisque. Vivamus pulvinar ultrices neque. Proin
+      molestie diam sed commodo vehicula. Pellentesque at dignissim ex. Donec eu
+      arcu massa. Nunc scelerisque et nibh at volutpat. Nullam eget libero
+      pulvinar diam iaculis dictum. Vestibulum hendrerit non enim a.
+    </p>
+  </div>
 </section>
