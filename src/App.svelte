c17bbc430636db6ec9c88ef16c5e05847e946616
neonnskye <theneonnofficial@gmail.com>
2026-01-15 12:39:39 +0530

    Add Contact component

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import Hero from "./lib/Hero.svelte";
   import About from "./lib/About.svelte";
   import Portfolio from "./lib/Portfolio.svelte";
+  import Contact from "./lib/Contact.svelte";
 </script>
 
 <main class="mx-20">
@@ -10,4 +11,5 @@
   <Hero />
   <About />
   <Portfolio />
+  <Contact />
 </main>
---
+++ src/lib/Contact.svelte
@@ -0,0 +1,14 @@
+<section class="mx-auto mb-10 w-180 text-center">
+  <div>
+    <h2 class="font-playfair-display mb-5 text-2xl tracking-wide uppercase">
+      Contact
+    </h2>
+  </div>
+  <div class="font-raleway">
+    <a href="mailto:info@untitledfurniture.com" class="underline"
+      >info@untitledfurniture.com</a
+    >
+    <p>Tel: 123-456-7890</p>
+    <p>500 Unknown St. Unknown City, CA 94158</p>
+  </div>
+</section>
