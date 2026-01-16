904368a9066066f84d314ab9c2ff1882d8904952
neonnskye <theneonnofficial@gmail.com>
2026-01-16 12:25:47 +0530

    Add navigation links to Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,14 +1,14 @@
 <script>
   const navLinks = [
     { href: "#top", label: "Home" },
-    { href: "#top", label: "About" },
-    { href: "#top", label: "Portfolio" },
-    { href: "#top", label: "Contact" },
+    { href: "#about", label: "About" },
+    { href: "#gallery", label: "Gallery" },
+    { href: "#contact", label: "Contact" },
   ];
 </script>
 
 <nav class="font-playfair-display my-5 flex justify-between text-lg uppercase">
-  <div class="">
+  <div>
     <h2 class="tracking-widest">Untitled Furniture</h2>
   </div>
   <div class="text-gray tracking-wide">
